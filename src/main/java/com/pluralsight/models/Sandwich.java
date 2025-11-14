7aefd9d302800c973f14a2142a1bcfa160ebd857
Malikc12 <canteberryma@gmail.com>
2025-11-14 05:43:14 -0600

    sandwich and topping completed

--- src/main/java/com/pluralsight/models/Sandwich.java
+++ src/main/java/com/pluralsight/models/Sandwich.java
@@ -7,13 +7,10 @@ import java.util.List;
 
 public class Sandwich implements UserInterface {
 
-
-
-
-    //private List<Topping> toppings;
+    private List<Topping> toppings;
     private  String bread;
-    private final String  size;
-    private double price;
+    private  String  size;
+    //private double price;
     private boolean isToasted;
     private boolean extraMeat;
     private boolean extraCheese;
@@ -25,22 +22,123 @@ public class Sandwich implements UserInterface {
         this.isToasted = isToasted;
         this.extraMeat = extraMeat;
         this.extraCheese = extraCheese;
-        //this.toppings
+        this.toppings = toppings;
     }
 
 
+    public List<Topping> getToppings() {
+        return toppings;
+    }
 
+    public void setToppings(List<Topping> toppings) {
+        this.toppings = toppings;
+    }
 
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public boolean isToasted() {
+        return isToasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        isToasted = toasted;
+    }
+
+    public boolean isExtraMeat() {
+        return extraMeat;
+    }
+
+    public void setExtraMeat(boolean extraMeat) {
+        this.extraMeat = extraMeat;
+    }
+
+    public boolean isExtraCheese() {
+        return extraCheese;
+    }
+
+    public void setExtraCheese(boolean extraCheese) {
+        this.extraCheese = extraCheese;
+    }
+
+    //price math
     @Override
     public double getPrice() {
         double price = 0;
+
+        //base price
         if (size.equals("4")) price += 5.50;
         else if (size.equals("8")) price += 7.00;
         else if (size.equals("12")) price += 8.50;
-            
-        for (String Topping : toppings) {
-            String lower = topping.toLowerCase();
 
-        if (lower.equals)
+        //loop for toppings
+        for (Topping topping : toppings) {
+            String type = topping.getType();
+        }
+
+
+        //meat
+        if (type.equals("meat")) {
+            if (size.equals("4")) price += 1.00;
+            else if (size.equals("8")) price += 2.00;
+            else price += 3.00;
+        }
+
+        //cheese
+        else if (type.equals("cheese")) {
+            if (size.equals("4")) price += 0.75;
+            else if (size.equals("8")) price += 1.50;
+            else price += 2.25
+        }
+
+        //extra meat
+        if (extraMeat) {
+            if (size.equals("4")) price += 0.50;
+            else if (size.equals("8")) price += 1.00;
+            else price += 1.50;
+        }
+
+        //extra cheese
+        if (extraCheese) {
+            if (size.equals("4")) price += 0.30;
+            else if (size.equals("8")) price += 0.60;
+            else price += 0.90;
+        }
+
+        return price;
+    }
+
+
+    @Override
+    public String toString() {
+        String output = "";
+
+        output += size + "\" " + bread;
+        if (isToasted) {
+            output += " (toasted)";
+        }
+        output += "\n";
+
+        for (Topping t : toppings) {
+            output += "  - " + t.toString() + "\n";
+        }
+
+        output += "Total: $" + String.format("%.2f", getPrice());
+
+        return output;
     }
 }
+
--- src/main/java/com/pluralsight/models/Topping.java
+++ src/main/java/com/pluralsight/models/Topping.java
@@ -1,46 +1,32 @@
 package com.pluralsight.models;
 
-import java.util.ArrayList;
-
 public class Topping {
 
     private String name;
-    private double price;
+    private String type;
     private boolean isExtra;
 
-    public Topping(String name, double price, boolean isExtra) {
-        this.name = name;
-        this.price = price;
+    public Topping(String name, String type, boolean isExtra) {
+        this.name = name.toLowerCase();
+        this.type = type.toLowerCase();
         this.isExtra = isExtra;
     }
 
-    public Topping(String name, double price) {
+    public Topping(String name, String type) {
         this.name = name;
-        this.price = price;
+        this.type = type;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
+    public String getType() {
+        return type;
     }
 
     public boolean isExtra() {
         return isExtra;
-    }
-
-    public void setExtra(boolean extra) {
-        isExtra = extra;
     }
 
     @Override
